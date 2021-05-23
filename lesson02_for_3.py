2a5a0a9762c4e15474d772f511fe7f140f3fd77f
Vladimir Bulatov <pioneerus38@gmail.com>
2021-05-24 00:04:33 +0800

    Внесены изменения по замечаниям

--- lesson02_for_3.py
+++ lesson02_for_3.py
@@ -2,10 +2,9 @@ school_scores = [
     {'school_class': '4a', 'scores': [3,4,4,5,2]},
     {'school_class': '4б', 'scores': [3,4,3,5,3]},
     {'school_class': '4в', 'scores': [4,4,5,5,3]},
-    {'school_class': '4г', 'scores': [3,4,4,4,5]}
+    {'school_class': '4г', 'scores': [3,4,4,4,5]},
+    {'school_class': '4д', 'scores': [3,4,3,3,3]}
 ] 
-# количество баллов: 2 * 1 + 3 * 6 + 4 * 8 + 5 * 5 = 77
-# количество оценок: 20
 
 def list_average(register):
     try:
@@ -14,9 +13,23 @@ def list_average(register):
         print("Ошибка: ожидается не пустой список целых чисел")
         return None
 
+# Вычисление через средние оценки по классам
 class_average_list = []
 
 for class_scores in school_scores:
-    class_average_list.append(list_average(class_scores["scores"]))
+    class_average = list_average(class_scores["scores"])
+    class_average_list.append(class_average)
+    print(f'Средняя оценка класса {class_scores["school_class"]}: {class_average}')
 
-print(list_average(class_average_list))
+print(f'Среднее значение средних оценок по классам: {list_average(class_average_list)}')
+
+# Вычисление средней оценки по общему списку школьных оценок
+school_scores_list = []
+
+for class_scores in school_scores:
+    school_scores_list.extend(class_scores["scores"])
+# Нашел интересный способ слияния двух списков через sorted. 
+# Вроде как самый быстрый, т.к. реализован на С.
+#    school_scores_list = sorted(school_scores_list + class_scores["scores"])
+ 
+print(f'Среднее значение оценок по школе: {list_average(school_scores_list)}')
--- lesson02_if_compare.py
+++ lesson02_if_compare.py
@@ -1,5 +1,5 @@
 def compare_strings(first_str, second_str):
-    if (type(first_str) != str) or (type(second_str) != str):
+    if not(isinstance(first_str, str)) or not(isinstance(second_str, str)):
         return 0
     elif first_str == second_str:
         return 1
@@ -7,7 +7,8 @@ def compare_strings(first_str, second_str):
         return 2
     elif second_str == "learn":
         return 3
-    return None # Explicit is better than implicit 
+    else:
+        return None # Explicit is better than implicit 
 
 print(compare_strings(True, "learn"))
 print(compare_strings("learn", "learn"))
